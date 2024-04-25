35c72d559834941b3c2ca94bc0f48565aceed3f8
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-04-25 11:59:13 -0400

    feat: Set new database connection

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 
-const USER = process.env.AWS_RDS_MASTER_USERNAME;
-const PASSWORD = process.env.AWS_RDS_MASTER_PASSWORD;
-const HOST = process.env.AWS_RDS_ENDPOINT;
-const PORT = process.env.AWS_RDS_PORT;
-const DBNAME = process.env.AWS_RDS_DATABASENAME;
-module.exports = { USER, PASSWORD, HOST, PORT, DBNAME };
+const DB_URL = process.env.DB_URL;
+const PORT = process.env.PORT;
+
+module.exports = { DB_URL, PORT };
