af6d3bf79aa4679ed2075d0fcc03b76aba9ccd60
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-02 11:02:24 -0400

    fix: Add type for createTable function

--- backend/src/db/migrations/20240430_00_initialize_users.ts
+++ backend/src/db/migrations/20240430_00_initialize_users.ts
@@ -1,11 +1,12 @@
 import { DataTypes, QueryInterface } from "sequelize";
 import { sequelize } from "../index";
+import { User } from "../models/models.types";
 
 // Allows interaction with the database
 const queryInterface: QueryInterface = sequelize.getQueryInterface();
 
 export const up = async (): Promise<void> => {
-  await queryInterface.createTable("users", {
+  await queryInterface.createTable<User>("users", {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     firstName: { type: DataTypes.STRING, allowNull: false },
     lastName: { type: DataTypes.STRING, allowNull: false },
