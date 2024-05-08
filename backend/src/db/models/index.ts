6689b3166050d3c808b9a54a3dfa6d56519bd386
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-08 12:35:38 -0400

    feat: Add many-to-many connection between Users and Roles

--- backend/src/db/models/index.ts
+++ backend/src/db/models/index.ts
@@ -2,7 +2,8 @@ import { User } from "./Users";
 import { Project } from "./Projects";
 import { Coderfairs } from "./Coderfair";
 import { Judges } from "./Judges";
-
+import { Roles } from "./Roles";
+import { User_Roles } from "./User_Roles";
 User.hasMany(Project, {
   sourceKey: "id",
   foreignKey: "student_id",
@@ -26,4 +27,16 @@ Coderfairs.belongsToMany(User, {
   foreignKey: "coderfair_id",
 });
 
-export { User, Project, Coderfairs, Judges };
+User.belongsToMany(Roles, {
+  through: User_Roles,
+  sourceKey: "id",
+  foreignKey: "user_id",
+});
+
+Roles.belongsToMany(User, {
+  through: User_Roles,
+  sourceKey: "id",
+  foreignKey: "role_id",
+});
+
+export { User, Project, Coderfairs, Judges, Roles, User_Roles };
