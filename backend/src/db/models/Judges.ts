fb22db5c2726cac251f0308cbc4ceecd790b2d88
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-08 12:13:28 -0400

    feat: Create Judges init file

---
+++ backend/src/db/models/Judges.ts
@@ -0,0 +1,17 @@
+import { Judges } from "./models.types";
+import { sequelize } from "../index";
+import { DataTypes } from "sequelize";
+
+Judges.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+  },
+  { sequelize, underscored: true, timestamps: false, modelName: "judges" }
+);
+
+export { Judges };
