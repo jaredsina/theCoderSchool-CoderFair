64608755e9756ab62e91ac0584ededf89e1e19c4
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-10 11:19:55 -0400

    feat: Setup basic express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,29 @@
 import dotenv from "dotenv";
 import { connectToDatabase } from "./db/index";
 import { SERVER_PORT } from "./utils/config";
+// no try/catch blocks for async/await
+import "express-async-errors";
+import express from "express";
+import { errorHandler } from "./middleware/errorHandler";
+import { requestLogger } from "./middleware/requestLogger";
+import { unknownEndpoint } from "./middleware/unknownEndpoint";
 
 dotenv.config();
 
-// no try/catch blocks for async/await
-import "express-async-errors";
-
-import express from "express";
-
 const app = express();
 app.use(express.json());
+
+// Easily view request info on the server logs
+app.use(requestLogger);
+
+// Assert as RequestHandler because post expects a void return not a Promise<void>
+app.get("/", (_req, res) => {
+  res.status(200).send("<h1>Hello World! I am Alive!</h1>");
+});
+
+// Handle any async errors or unknownEndpoints
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const start = async () => {
   // We want to make sure that the database connection is established successfully before the actual startup.
