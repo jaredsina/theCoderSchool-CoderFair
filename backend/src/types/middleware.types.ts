fc251555f03aa6dde6cf10bc54118607595105eb
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-13 12:57:54 -0400

    feat: Update Request type to accept tokens and decodedTokens

--- backend/src/types/middleware.types.ts
+++ backend/src/types/middleware.types.ts
@@ -1,7 +1,9 @@
 import { Request } from "express";
+import { JwtPayload } from "jsonwebtoken";
 
 type Token = {
-  token: string;
+  token?: string;
+  decodedToken?: string | JwtPayload;
 };
 
 export type RequestToken = Request & Token;
