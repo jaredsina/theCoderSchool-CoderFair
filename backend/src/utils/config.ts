c86d667f993e54b883418c6fc94b98b0bb23b516
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-13 12:05:44 -0400

    feat: Add SECRET for password hashing

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -8,17 +8,25 @@ const isPort = (port: unknown): port is number => {
 const parsePort = (port: unknown): number => {
   const portNumber = Number(port);
   if (!port || !isPort(portNumber)) {
-    throw new Error("Incorrect or missing PORT" + port);
+    throw new Error("Incorrect or missing PORT " + port);
   }
   return portNumber;
 };
 
 const parseDbUrl = (url: unknown): string => {
   if (!url || !(typeof url === "string")) {
-    throw new Error("Incorrect or missing URL" + url);
+    throw new Error("Incorrect or missing URL " + url);
   }
   return url;
 };
 
+const parseSecret = (secret: unknown): string => {
+  if (!secret || !(typeof secret === "string")) {
+    throw new Error("Incorrect or missing jwt SECRET " + secret);
+  }
+  return secret;
+};
+
 export const DATABASE_URL: string = parseDbUrl(process.env.DATABASE_URL);
 export const SERVER_PORT: number = parsePort(process.env.SERVER_PORT);
+export const SECRET: string = parseSecret(process.env.SECRET);
