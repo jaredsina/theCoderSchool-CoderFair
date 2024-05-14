11310a0bd478d88b1d3acb4e63e82d103a709bbd
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-14 12:07:05 -0400

    refactor: Move isString and object checker to own modules

--- backend/src/utils/login.utils.ts
+++ backend/src/utils/login.utils.ts
@@ -1,22 +1,12 @@
 import { newLogin } from "../types/login.types";
-
-// Type guard for username and password
-const isString = (value: unknown): string => {
-  if (typeof value === "string") {
-    return value;
-  }
-  throw new Error("Incorrect data: username or password");
-};
+import { isObject, parseString } from "./index.utils";
 
 // Type narrow for the request body of a login
 export const toNewLogin = (object: unknown): newLogin => {
-  if (!object || typeof object !== "object") {
-    throw new Error("Incorrect or missing data" + object);
-  }
-  if ("username" in object && "password" in object) {
+  if (isObject(object) && "username" in object && "password" in object) {
     const newLogin: newLogin = {
-      username: isString(object.username),
-      password: isString(object.password),
+      username: parseString(object.username),
+      password: parseString(object.password),
     };
     return newLogin;
   }
