59e285ceabb98fc43dc45281d4bc5eed16324dad
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-15 12:17:23 -0400

    feat: Create endpoint for creating new coderfairs

--- backend/src/routes/coderfair.routes.ts
+++ backend/src/routes/coderfair.routes.ts
@@ -1,9 +1,18 @@
 import { Request, RequestHandler, Response, Router } from "express";
 import { Coderfairs } from "../db/models/index";
+import { toNewCoderfair } from "../utils/coderfairs.util";
 
 export const coderfairRouter = Router();
 
 coderfairRouter.get("/", (async (_req: Request, res: Response) => {
   const coderfairs = await Coderfairs.findAll();
   res.status(200).json(coderfairs);
+}) as RequestHandler);
+
+coderfairRouter.post("/", (async (req: Request, res: Response) => {
+  const newCoderfair = toNewCoderfair(req.body);
+
+  const uploadedCoderfair = await Coderfairs.create(newCoderfair);
+
+  res.status(200).json(uploadedCoderfair);
 }) as RequestHandler);
