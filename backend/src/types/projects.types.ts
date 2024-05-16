22084544020e3af8baf048e34acec4a0be20c3d7
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-16 10:49:49 -0400

    feat: Create type for a newproject

---
+++ backend/src/types/projects.types.ts
@@ -0,0 +1,12 @@
+export type newProject = {
+  name: string;
+  description?: string;
+  presentation_video_url?: string;
+  code_access_link: string;
+  coding_language: string;
+  project_username: string;
+  project_password: string;
+  notes?: string;
+  student_id: number;
+  coderfair_id: number;
+};
