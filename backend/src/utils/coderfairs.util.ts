908a7ac2ea297fa4a594a7a9ac7e38135d07dd98
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-16 10:45:51 -0400

    feat: Add name to validation check

--- backend/src/utils/coderfairs.util.ts
+++ backend/src/utils/coderfairs.util.ts
@@ -2,8 +2,9 @@ import { CoderFair } from "../types/coderfairs.types";
 import { isObject, parseDate, parseString } from "./index.utils";
 
 export const toNewCoderfair = (object: unknown): CoderFair => {
-  if (isObject(object) && "fairDate" in object) {
+  if (isObject(object) && "fairDate" in object && "name" in object) {
     const newCoderfair: CoderFair = {
+      name: parseString(object.name),
       fairDate: parseDate(object.fairDate),
       description:
         "description" in object ? parseString(object.description) : undefined,
