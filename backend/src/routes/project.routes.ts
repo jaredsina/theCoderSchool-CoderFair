e1e368ab38a5bf6ca6a72ef573edd67d2d22c1e1
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-16 11:10:42 -0400

    feat: Create post new projects endpoint for logged in users

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -1,9 +1,34 @@
 import { RequestHandler, Response, Router } from "express";
 import { Project } from "../db/models/index";
-
+import { toNewProject } from "../utils/projects.util";
+import bcrypt from "bcrypt";
+import { tokenExtractor } from "../middleware/tokenExtractor";
+import { tokenValidator } from "../middleware/tokenValidator";
+import { RequestToken } from "../types/middleware.types";
+import { parseDecodedToken } from "../utils/user.utils";
 export const projectRouter = Router();
 
 projectRouter.get("/", (async (_req, res: Response) => {
   const projects = await Project.findAll();
   return res.status(200).json(projects);
+}) as RequestHandler);
+
+projectRouter.post("/", tokenExtractor, tokenValidator, (async (
+  req: RequestToken,
+  res: Response
+) => {
+  const newProject = toNewProject(req.body);
+  const saltRounds: number = 10;
+  const passwordHash = await bcrypt.hash(
+    newProject.project_password,
+    saltRounds
+  );
+  const { id } = parseDecodedToken(req.decodedToken);
+  const uploadedProject = await Project.create({
+    ...newProject,
+    project_password: passwordHash,
+    student_id: id,
+  });
+
+  res.status(200).json(uploadedProject);
 }) as RequestHandler);
