fd47f23b9ab267927fde59fa8a5e6df6a8de3e1e
Sina Hernandez <48105264+jaredsina@users.noreply.github.com>
2024-05-21 12:43:53 -0400

    feat: Add prettier to eslint config

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,23 +1,16 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: ["eslint:recommended"],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-  },
+  env: { node: true, es2020: true },
+  extends: ['eslint:recommended'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules/*'],
   overrides: [
     {
-      files: ["**/*.ts", "**/*.tsx"],
-      parser: "@typescript-eslint/parser",
+      plugins: ['react-refresh'],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
       settings: {
-        react: { version: "detect" },
-        "import/resolver": {
+        react: { version: 'detect' },
+        'import/resolver': {
           typescript: {},
         },
       },
@@ -27,36 +20,42 @@ module.exports = {
         es2020: true,
       },
       extends: [
-        "eslint:recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
       ],
       rules: {
-        "import/no-restricted-paths": [
-          "error",
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: true },
+        ],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'import/no-restricted-paths': [
+          'error',
           {
             zones: [
               // enforce unidirectional codebase:
 
               // e.g. src/app can import from src/features but not the other way around
               {
-                target: "./src/features",
-                from: "./src/app",
+                target: './src/features',
+                from: './src/app',
               },
 
               // e.g src/features and src/app can import from these shared modules but not the other way around
               {
                 target: [
-                  "./src/components",
-                  "./src/hooks",
-                  "./src/lib",
-                  "./src/types",
-                  "./src/utils",
+                  './src/components',
+                  './src/hooks',
+                  './src/lib',
+                  './src/types',
+                  './src/utils',
                 ],
-                from: ["./src/features", "./src/app"],
+                from: ['./src/features', './src/app'],
               },
             ],
           },
